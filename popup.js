2176db5227f302cc38b2fe1903dd7617dca9bf32
anjali-dhanuka <anjalidhanuka99@gmail.com>
2017-09-03 04:54:23 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
-//var json;
 var t;
-var i;
+var i=0;
 var start;
 var upcoming_contests=[];
-var ongoing_contest={items:[]};
+//var ongoing_contest={items:[]};
 var d=new Date();
+var offset = d.getTimezoneOffset();
+console.log(d);
 var n=d.getTime();
-var FetchedContestUrls = {};
-console.log(n);
-function contests()
-{
-	i=0;
-	console.log("call");
+//var FetchedContestUrls = {};
+	function notice()
+  {
+chrome.browserAction.setBadgeText({text: ""});
+  }
+  console.log("call");
 	$.getJSON("http://codeforces.com/api/contest.list",function(data){
      // json = data.result;
 	$.each(data.result, function(key, val) {
@@ -19,22 +20,33 @@ function contests()
    t=(val.startTimeSeconds+val.durationSeconds)*1000;
    if(n<t)
    {
-   	//console.log("fudhs");
+   	console.log("fudhs");
    	if(n<val.startTimeSeconds*1000)
    	{
-   	upcoming_contests[i]=val;
-   //alert(start);
-   i+=1;
+      console.log("ndhsa");
+      //console.log((start-n)/60000);
+    var diff=start-d;
+    console.log(diff/3600000);
+    if((diff/3600000)==12)
+    {
+    console.log("gyfrh");
+    setTimeout(notice(),start-d-3600000);
+     }
+    //console.log((diff/1000)%24);
+     //i=1;
+    //chrome.browserAction.setBadgeText({text:""});
    console.log("up");
-   $("#u").append("<a href='http://codeforces.com/contests/"+val.id+"/'>Click here</a>");
+   $("#u").append("<a href='http://codeforces.com/contests/"+val.id+"/'>*</a>");
    $("#u").append(val.name);
    $("#u").append("<br>");
+   //$("#u").append(val.startTimeSeconds);
    $("#u").append(start);
    $("#u").append("<br>");
    $("#u").append("Duration in min : ");
    $("#u").append(val.durationSeconds/60);
    $("#u").append("<br><br>");
 	}
+
    	else
    	{
    $("#o").append("<a href='http://codeforces.com/contests/"+val.id+"/'>Click here</a>");
@@ -45,18 +57,8 @@ function contests()
    $("#u").append("Duration in min : ");
   $("#o").append(val.durationSeconds/60);
   $("#o").append("<br><br>");
-   	}
+   	} 
    }
 });
 });
-}
-
-for(var j=0;j<i;j++)
-{
-	if(((upcoming_contests[i].startTimeSeconds-n/1000)/3600)==1)
-	{
-      chrome.browserAction.setBadgeText({text:""});		
-	}
-}
-
- 
+  
